6595f1b9c20eb11ce7cd1b3a2a5e1c753bf9e441
Jakob Segerslätt <segerslatt@gmail.com>
2019-02-20 08:37:13 +0100

    updated scripts

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Timer } from './timer';
-export { clearNulls, clone, createGuid, contains, convertToTypedClass, flattenTree, groupArrayByProperty, isNotNullorEmpty, isObject, pascalCase, removeDuplicates, reverseEnum, Storage, throwIfAlreadyLoaded, trimString } from './helper-functions';
